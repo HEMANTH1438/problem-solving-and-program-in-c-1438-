784f98e21a509562d96f377a39f3b6f9a912d36f
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-11-25 19:05:50 +0530

    Swapping in programming in .c

---
+++ Swapping in programming in c (6).cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=10;b=20;c=30;
+	a=b;b=c;c=a;
+	printf("\n The value of a is %d",a);
+	printf("\n The value of b is %d",b);
+	printf("\n The value of c is %d",c);
+
+}
