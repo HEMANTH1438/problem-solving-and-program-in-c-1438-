1d13029bf7dd50e86802c2cb4605dd0ef1041ece
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-11-25 10:07:58 +0530

    Add files via upload

---
+++ All arithematic values in one programm in c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e;
+	float f;
+	a=5;b=8;
+	c=a+b;
+	d=a*b;
+	e=a-b;
+	f=a/b;
+	printf("The addition,multiplication,substraction and division respectively is:%d,%d,%d,%f",c,d,e,f);
+}
