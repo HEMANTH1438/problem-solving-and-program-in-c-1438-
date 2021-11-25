3808fec96f9acc3bb685e94766209ae756ce019c
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-11-25 19:09:47 +0530

    To find you are eligible to vote or not in.c

---
+++ To find you are eligible to vote or not (1).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter your age");
+	scanf("%d",&a);
+	if (a>=18)
+{
+	printf("Your are eligible to vote");}
+	else
+	{
+	printf("You are not eligible to vote");}
+}
