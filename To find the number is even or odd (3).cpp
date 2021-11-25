e3648078af1f441fd53e02133068fb6e2c708d61
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-11-25 19:08:54 +0530

    To find the number is even or odd in.c

---
+++ To find the number is even or odd (3).cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter your roll number");
+	scanf("%d",&a);
+	if (a%2==0)
+{
+	printf("Your number is even");}
+	else
+	{
+	printf("Your number is odd");}
+}
